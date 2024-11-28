2a0b26fd4656802f1b127abd41a7d03dd44d53df
Parminder Singh <yesask8@gmail.com>
2024-11-28 21:56:57 +0530

    initialize React project with Vite, Tailwind CSS, and ESLint setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import './App.css'
+import Loader from './Components/Loader'
+
+function App() {
+
+  return (
+    <>
+    <div className=' bg-white'>
+
+    <Loader />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { motion } from "motion/react"
+
+const Loader = () => {
+    return (
+        <motion.div
+            initial={{ opacity: 1 ,y: 0 }}
+            animate={{ opacity: 0,y: 1000 }}
+            transition={{ duration: 3.5,delay: 5 }}
+            className="flex items-center justify-center h-screen w-screen bg-[#242424]">
+            <motion.h1 
+            initial={{ opacity: 0,z: 0 }}
+            animate={{ opacity: 1,z: 100 }}
+            transition={{ duration: 8 }}
+            
+            className="text-4xl font-bold">Swasthya</motion.h1>
+        </motion.div>
+    );
+};
+
+export default Loader;
